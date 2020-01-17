2a1d4c83e5b4850b88fd3d51afba05926c3d43ff
VladShchetyna <sch.vladislav24@gmail.com>
2020-01-17 13:57:23 +0200

    common elements added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
 import './App.css';
+
+import Dashboard from "./pages/dashboardPage/DashboardPage";
+import SignIn from "./pages/SignInPage/SignInPage";
+import SignUp from "./pages/SignUpPage/SignUpPage";
 
 function App() {
   return (
-    <div className="App">
-
-    </div >
+    <BrowserRouter>
+      <div className="App">
+        <Route path="/" exact component={SignIn} />
+        <Route path="/signup" exact component={SignUp} />
+        <Route path="/dashboard" exact component={Dashboard} />
+      </div >
+    </BrowserRouter>
   );
 }
 
